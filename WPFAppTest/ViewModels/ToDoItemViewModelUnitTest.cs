5ef8512c4e3629a3bb07ecdd218c7bb55bbc9ac4
Zille Huma <Zille.Huma@krones.com>
2021-10-21 12:36:56 +0200

    First Commit for ToDoITemViewModelUNitTest

--- WPFAppTest/ViewModels/ToDoItemViewModelUnitTest.cs
+++ WPFAppTest/ViewModels/ToDoItemViewModelUnitTest.cs
@@ -1,14 +1,44 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp.Models;
+using WpfApp.ViewModels;
 
 namespace WPFAppTest
 {
     [TestClass]
     class ToDoItemViewModelUnitTest
     {
+        [TestMethod]
+        public void SetIsDone_IsDoneIsTrue_WriteToDoItemsExecuted()
+        {
+            //Arrange
+            var fakeTodoService = new FakeTodoService();            
+            var viewModel = this.CreateSut(fakeTodoService);
+
+            //Act
+
+            viewModel.IsDone = true;
+
+            //Assert
+            //fakeTodoService
+        }
+        private ToDoItemViewModel CreateSut(FakeTodoService fakeTodoService = null)
+        {
+            if(fakeTodoService ==null)
+            {
+                fakeTodoService = new FakeTodoService();
+            }
+
+            var todoitem = new TODOItem();
+            var alltodos = new BindingList<ToDoItemViewModel>();
+
+
+            return new ToDoItemViewModel(todoitem, fakeTodoService, alltodos);
+        }
     }
 }
